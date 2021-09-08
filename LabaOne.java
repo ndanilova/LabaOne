b55907905b6accd364c82c4658d0fdfa962973a1
Anastasia <apple.for.ledi@icloud.com>
2021-09-08 21:03:42 +0300

    1.2

---
+++ LabaOne.java
@@ -0,0 +1,12 @@
+        class LabaOne {
+            public static void main(String[] args) {
+                short[] a = new short[]{17,16,15,14,13,12,11,10,9,8,7,6,5,4,3};
+                //System.out.println(a[5]);
+                float[] x = new float[17];
+                for (int i = 0; i < x.length; i++){
+                   x[i] = (float)(Math.random() * (3+14) -14);
+                    //System.out.println(x[i]);
+                }
+
+                }
+        }
