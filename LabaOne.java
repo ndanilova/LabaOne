301ef5e27d7d4ae08d6b8d7b6e209a060f07480a
Anastasia <apple.for.ledi@icloud.com>
2021-09-11 10:39:56 +0300

    3.0
    final final version

--- LabaOne.java
+++ LabaOne.java
@@ -3,32 +3,34 @@
             public static void main(String[] args)
                 {
                   short[] a;
-                  a =new short[15];
-                  for (int i = 14; i>-1;i--)
+                  a =new short[20];
+                  for (int i = 0; i<20;i++)
                     {
-                      a[i]= (short)(i+3);
+                      a[i]= (short)(i+1);
+
                     }
                   double[] x;
-                  x = new double [17];
-                  for (int i = 0; i < 17; i++)
+                  x = new double [12];
+                  for (int i = 0; i < 12; i++)
                     {
-                      x[i] = (double)(Math.random()*17 -14);
+                      x[i] = (double)(Math.random()*24 -10);
                     }
-                  double[][] e = new double [8][17];
-                  for (int i = 0; i< 8; i++)
+                  double[][] e = new double [20][12];
+                  for (int i = 0; i< 20; i++)
                     {
-                      for(int j = 0; j<17;j++)
+                      for(int j = 0; j<12;j++)
                         {
-                          if(a[i]==11) e[i][j] = Math.pow((Math.asin(1/Math.pow(Math.E,Math.abs(x[j])))*(Math.cbrt(Math.atan((x[j]-5.5)/17))-1)),Math.cbrt(Math.pow(x[j],x[j]/2)));
-                          else if (a[i]==3|a[i]==5|a[i]==7|a[i]==17) e[i][j]= Math.tan(Math.atan(Math.pow((x[j]-5.5)/17,2)));
-                          else e[i][j]= 0.75/(4+Math.cos(Math.pow(0.25+(x[j]-1)/x[j],2)));
+                          if(a[i]==8) e[i][j] = Math.asin(Math.sin(Math.pow(Math.asin((x[j]+2)/24),0.5*x[j])));
+                          else if (a[i]==2|a[i]==3|a[i]==4|a[i]==5|a[i]==6|a[i]==9|a[i]==13|a[i]==15|a[i]==17|a[i]==20) e[i][j]= Math.pow(Math.atan(Math.cos(x[j])),2*Math.tan((1/3)/(x[j]+1)));
+                          else e[i][j]= Math.cbrt(Math.pow(Math.cbrt(Math.pow(Math.E,x[j])),Math.pow(0.75*Math.asin((x[j]+2)/24),Math.pow((1/3)*x[j]/Math.PI,2))));
+
                         }
                     }
-                    for ( int i = 0; i<8;i++)
+                    for ( int i = 0; i<20;i++)
                       {
-                        for(int j=0;j<17;j++)
+                        for(int j=0;j<12;j++)
                           {
-                            System.out.printf("%.2f", e[i][j]);
+                            System.out.printf("%.5f", e[i][j]);
                             System.out.print(" ");
                           }
                           System.out.println();
