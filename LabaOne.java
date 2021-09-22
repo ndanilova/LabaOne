a1cc714df23a675e07684d1a7199c74f127c0429
ndanilova <70074699+ndanilova@users.noreply.github.com>
2021-09-22 10:15:47 +0300

    Add files via upload

--- LabaOne.java
+++ LabaOne.java
@@ -2,38 +2,59 @@
         {
             public static void main(String[] args)
                 {
+                  final int ALEN = 20;
+                  final int XLEN = 12;
+                  final int ELEN =20;
+                  final int EHEIGHT= 12;
+
                   short[] a;
-                  a =new short[20];
-                  for (int i = 0; i<20;i++)
+                  a =new short[ALEN];
+                  //a - массив 20 чисел по возрастанию [1;20]
+                  for (int i = 0; i<ALEN;i++)
                     {
                       a[i]= (short)(i+1);
 
                     }
                   double[] x;
-                  x = new double [12];
-                  for (int i = 0; i < 12; i++)
+                  x = new double [XLEN];
+                  //x - массив 12 ранд. чисел [-10;14]
+                  for (int i = 0; i < XLEN; i++)
                     {
-                      x[i] = (double)(Math.random()*24 -10);
+                      x[i] = randomiser(Math.random());
                     }
-                  double[][] e = new double [20][12];
-                  for (int i = 0; i< 20; i++)
+                  double[][] e = new double [ELEN][EHEIGHT];
+                  //e - массив 20х12
+                  for (int j = 0; j< EHEIGHT; j++)
                     {
-                      for(int j = 0; j<12;j++)
+                      for(int i = 0; i<ELEN;i++)
                         {
-                          if(a[i]==8) e[i][j] = Math.asin(Math.sin(Math.pow(Math.asin((x[j]+2)/24),0.5*x[j])));
-                          else if (a[i]==2|a[i]==3|a[i]==4|a[i]==5|a[i]==6|a[i]==9|a[i]==13|a[i]==15|a[i]==17|a[i]==20) e[i][j]= Math.pow(Math.atan(Math.cos(x[j])),2*Math.tan((1/3)/(x[j]+1)));
-                          else e[i][j]= Math.cbrt(Math.pow(Math.cbrt(Math.pow(Math.E,x[j])),Math.pow(0.75*Math.asin((x[j]+2)/24),Math.pow((1/3)*x[j]/Math.PI,2))));
+                          if(a[i]==8) e[i][j] = cond1(x[j]);
+                          else if (a[i]==2|a[i]==3|a[i]==4|a[i]==5|a[i]==6|a[i]==9|a[i]==13|a[i]==15|a[i]==17|a[i]==20) e[i][j]= cond2(x[j]);
+                          else e[i][j]= cond3(x[j]);
 
                         }
                     }
-                    for ( int i = 0; i<20;i++)
+                    for ( int j = 0; j<EHEIGHT;j++)
                       {
-                        for(int j=0;j<12;j++)
+                        for(int i=0;i<ELEN;i++)
                           {
                             System.out.printf("%.5f", e[i][j]);
                             System.out.print(" ");
                           }
                           System.out.println();
                       }
+                }
+                public static double randomiser(double rand){
+                   return (double)(Math.random()*24 -10);
+
+                }
+                public static double cond1(double vmestox1){
+                  return Math.asin(Math.sin(Math.pow(Math.asin((vmestox1+2)/24),0.5*vmestox1)));
+                }
+                public static double cond2(double vmestox2){
+                  return Math.pow(Math.atan(Math.cos(vmestox2)),2*Math.tan((1/3)/(vmestox2+1)));
+                }
+                public static double cond3(double vmestox3) {
+                  return Math.cbrt(Math.pow(Math.cbrt(Math.pow(Math.E,vmestox3)),Math.pow(0.75*Math.asin((vmestox3+2)/24),Math.pow((1/3)*vmestox3/Math.PI,2))));
                 }
         }
