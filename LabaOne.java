c10ed17cb61be15e61064bcc1df0ccd5a8d6d2df
ndanilova <70074699+ndanilova@users.noreply.github.com>
2021-10-05 15:45:29 +0300

    Add files via upload

--- LabaOne.java
+++ LabaOne.java
@@ -1,61 +1,77 @@
-        class LabaOne
-        {
-            public static void main(String[] args)
-                {
-                  final int ALEN = 20;
-                  final int XLEN = 12;
-                  final int ELEN =12;
-                  final int EHEIGHT= 20;
 
-                  short[] a;
-                  a =new short[ALEN];
-                  //a - массив 20 чисел по возрастанию [1;20]
-                  for (int i = 0; i<ALEN;i++)
-                    {
-                      a[i]= (short)(i+1);
+class LabaOne {
 
-                    }
-                  double[] x;
-                  x = new double [XLEN];
-                  //x - массив 12 ранд. чисел [-10;14]
-                  for (int i = 0; i < XLEN; i++)
-                    {
-                      x[i] = randomiser(Math.random());
-                    }
-                  double[][] e = new double [EHEIGHT][ELEN];
-                  //e - массив 20х12
-                  for (int i = 0; i< EHEIGHT; i++)
-                    {
-                      for(int j = 0; j<ELEN;j++)
-                        {
-                          if(a[i]==8) e[i][j] = cond1(x[j]);
-                          else if (a[i]==2||a[i]==3||a[i]==4||a[i]==5||a[i]==6||a[i]==9||a[i]==13||a[i]==15||a[i]==17||a[i]==20) e[i][j]= cond2(x[j]);
-                          else e[i][j]= cond3(x[j]);
+  static final int ALEN = 20;
+    static final int XLEN = 12;
+    static final int ELEN = 12;
+    static final int EHEIGHT = 20;
 
-                        }
-                    }
-                    for ( int i = 0; i<EHEIGHT;i++)
-                      {
-                        for(int j=0;j<ELEN;j++)
-                          {
-                            System.out.printf("%.5f", e[i][j]);
-                            System.out.print(" ");
-                          }
-                          System.out.println();
-                      }
-                }
-                public static double randomiser(double rand){
-                   rand = (Math.random()*24 -10);
-                   return rand;
+      public static void main(String[] args) {
+          double[][] e = matrixFiller();
 
-                }
-                public static double cond1(double vmestox1){
-                  return Math.asin(Math.sin(Math.pow(Math.asin((vmestox1+2)/24),0.5*vmestox1)));
-                }
-                public static double cond2(double vmestox2){
-                  return Math.pow(Math.atan(Math.cos(vmestox2)),2*Math.tan((1/3d)/(vmestox2+1)));
-                }
-                public static double cond3(double vmestox3) {
-                  return Math.cbrt(Math.pow(Math.cbrt(Math.pow(Math.E,vmestox3)),Math.pow(0.75*Math.asin((vmestox3+2)/24),Math.pow((1/3)*vmestox3/Math.PI,2))));
-                }
-        }
+          arrayPrinter(e);
+      }
+
+      public static double randomiser() {
+          return Math.random() * 24 - 10;
+      }
+
+      public static double cond1(double vmestox1) {
+          return Math.asin(Math.sin(Math.pow(Math.asin((vmestox1+2d)/24d),0.5d*vmestox1)));
+      }
+
+      public static double cond2(double vmestox2) {
+          return Math.pow(Math.atan(Math.cos(vmestox2)),2d*Math.tan((1/3d)/(vmestox2+1d)));
+      }
+
+      public static double cond3(double vmestox3) {
+          return Math.cbrt(Math.pow(Math.cbrt(Math.pow(Math.E,vmestox3)),
+                                    Math.pow(0.75d*Math.asin((vmestox3+2d)/24d),
+                                             Math.pow((1/3d)*vmestox3/Math.PI,2))));
+      }
+
+      public static short[] fillArray() {
+          short[] arrayIncrease = new short[ALEN];
+          for (int anum = 0; anum < arrayIncrease.length ; anum++) {
+              arrayIncrease[anum] = (short)(anum + 1);
+
+          }
+          return arrayIncrease;
+      }
+
+       public static double[][] matrixFiller(){
+          double[][] metMatrix = new double [EHEIGHT][ELEN];
+          short[] afiller = fillArray();
+          double[] xfiller = new double[XLEN];
+
+          for (int i = 0; i < xfiller.length; i++){
+            xfiller[i] = randomiser();
+          }
+
+          for (int i = 0; i < metMatrix.length; i++){
+              for (int j = 0; j < metMatrix[i].length; j++){
+                  if (afiller[i] == 8) {
+                      metMatrix[i][j] = cond1(xfiller[j]);
+                  } else if (afiller[i] == 2 || afiller[i] == 3 || afiller[i] == 4 || afiller[i] == 5 ||
+                             afiller[i] == 6 || afiller[i] == 9 || afiller[i] == 13 || afiller[i] == 15 ||
+                             afiller[i] == 17 || afiller[i] == 20) {
+                      metMatrix[i][j] = cond2(xfiller[j]);
+                  } else {
+                      metMatrix[i][j] = cond3(xfiller[j]);
+                  }
+              }
+          }
+
+          return metMatrix;
+      }
+
+      public static void arrayPrinter(double[][] earray){
+          for (int i = 0; i < earray.length; i++){
+              for (int j = 0; j < earray[i].length; j++){
+                  System.out.printf("%.5f ", earray[i][j]);
+              }
+            System.out.println();
+          }
+      }
+
+}
